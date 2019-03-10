95b0ea9c5a791df3e4bb9ba6d7ea808bd27090a6
Christina McMahon <christinamcmahon@hungtraanMac.localdomain>
2019-03-09 17:15:58 -0800

    Add Board and main

---
+++ Board.py
@@ -0,0 +1,16 @@
+# Board.py
+# Board class
+
+class Board:
+
+    TURTLES = [
+        Turtle('red'),
+        Turtle('green'),
+        Turtle('blue'),
+        Turtle('yellow'),
+        Turtle('white')]
+
+    def __init__(self):
+
+    
+        
---
+++ main.py
@@ -0,0 +1,31 @@
+# main.py
+# Turtle Race card game
+
+# Description of game and rules
+print("TURTLE RACE")
+print("Play cards to move turtles. First turtle to pass the finish line wins.")
+
+# Ask for number of players
+numPlayers = input("Enter number of players:")
+
+# Tell players to choose random colors and order of players
+print("Randomly determine players' turtle colors and first player.")
+
+# Initialize game with all turtles starting at [0]
+board = Board()
+
+# Distribute five cards to each player
+
+# Loop over player turns
+
+    # Play a card
+
+    # Move turtle according to card played
+
+    # If there is a winner, end game
+
+    # Pick up new card
+
+    # Go to next player
+
+# End game and announce winner
